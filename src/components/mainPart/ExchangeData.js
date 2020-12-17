d88cc44bfb96013aebbab995e494c6e3091c2dbe
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2020-12-17 18:08:21 +0300

    added exchange data

---
+++ src/components/mainPart/ExchangeData.js
@@ -0,0 +1,18 @@
+const ExchangeData = {
+  USD: "EGG",
+  CHF: "CABBAGE",
+  VND: "CART",
+  TND: "POTATO",
+  GGP: "APPLES",
+  BRL: "BREAD",
+  DKK: "CHEESE",
+  HRK: "VINE",
+  MVR: "PLATE",
+  MXN: "JAR",
+  CZK: "CHICKEN",
+  MMK: "PIG",
+  LAK: "COW",
+  GNF: "HORSE",
+};
+
+export default ExchangeData;
--- src/components/mainPart/MainPart.js
+++ src/components/mainPart/MainPart.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import './MainPart.css'
+import React from "react";
+import "./MainPart.css";
+import { Component } from "react";
+import ExchangeData from "./ExchangeData";
 
-export default function MainPart() {
-    return (
-        <div>
-            MAIN PART
-        </div>
-    )
+export default class MainPart extends Component {
+  state = {
+    data: {},
+    dataFromApiReceived: false,
+  };
+
+  apiUrl = `https://v6.exchangerate-api.com/v6/aa7daac21e6dccc5d465cd13/latest/USD`;
+  
+
+  componentDidMount() {
+    fetch(this.apiUrl)
+      .then((response) => response.json())
+      .then((dataFromApi) =>
+        this.setState({
+          data: dataFromApi.conversion_rates,
+          dataFromApiReceived: true,
+        })
+      );
+
+    this.setState({
+      dataFromApiReceived: false,
+    });
+  }
+
+  render() {
+    console.log(ExchangeData);
+    return <div>hello</div>;
+  }
 }
