59646559edec9922f70c3843f4b68602054d4b9f
Nadine <69895853+Nadine1111@users.noreply.github.com>
2020-12-17 21:05:02 +0100

    dropdown items named after icons

--- src/components/mainPart/MainPart.js
+++ src/components/mainPart/MainPart.js
@@ -51,21 +51,25 @@ export default function MainPart(props) {
                             setMyProduct(event.target.value);
                         }}
                     >
-                        <option value="EGG">egg</option>
-                        <option value="CABBAGE">cabbage</option>
-                        <option value="FARMER">
-                            services of a sexy farmer (1 hour)
-                        </option>
-                        <option value="POTATO">potato</option>
-                        <option value="APPLES">apples</option>
-                        <option value="BREAD">bread</option>
-                        <option value="WINE">wine</option>
-                        <option value="pLATE">beer mug</option>
-                        <option value="JAR">jar of "happy" mushrooms</option>
-                        <option value="CHICKEN">chicken</option>
-                        <option value="PIG">fat pig</option>
+                        <option value="CORN">corn</option>
+                        <option value="FARM">farm</option>
+                        <option value="FARMER">farmer</option>
+                        <option value="FARMER-GIRL">farmer-girl</option>
+                        <option value="PIG">pig</option>
                         <option value="COW">cow</option>
+                        <option value="CARROTS">carrots</option>
+                        <option value="CHICKEN">chicken</option>
+                        <option value="SHEEP">sheep</option>
                         <option value="HORSE">horse</option>
+                        <option value="EGGS">eggs</option>
+                        <option value="GOOSE">goose</option>
+                        <option value="WEATHER-VANE">weather-vane</option>
+                        <option value="HAY-BALE">hay-bale</option>
+                        <option value="SEXY-FARMER">services of sexy-farmer (1 hour)</option>
+                        <option value="FARM-LADY">farm-lady (not sure what you'll get)</option>
+                        <option value="PS5">PS5</option>
+                        <option value="SPACE-MONKEY">astro space-monkey</option>
+                        <option value="PIKACHU">pikachu</option>
                     </select>
                 </form>
             </div>
@@ -81,21 +85,25 @@ export default function MainPart(props) {
                             setDesiredProduct(event.target.value);
                         }}
                     >
-                        <option value="EGG">egg</option>
-                        <option value="CABBAGE">cabbage</option>
-                        <option value="FARMER">
-                            services of a sexy farmer (1 hour)
-                        </option>
-                        <option value="POTATO">potato</option>
-                        <option value="APPLES">apples</option>
-                        <option value="BREAD">bread</option>
-                        <option value="WINE">wine</option>
-                        <option value="pLATE">beer mug</option>
-                        <option value="JAR">jar of "happy" mushrooms</option>
-                        <option value="CHICKEN">chicken</option>
-                        <option value="PIG">fat pig</option>
+                        <option value="CORN">corn</option>
+                        <option value="FARM">farm</option>
+                        <option value="FARMER">farmer</option>
+                        <option value="FARMER-GIRL">farmer-girl</option>
+                        <option value="PIG">pig</option>
                         <option value="COW">cow</option>
+                        <option value="CARROTS">carrots</option>
+                        <option value="CHICKEN">chicken</option>
+                        <option value="SHEEP">sheep</option>
                         <option value="HORSE">horse</option>
+                        <option value="EGGS">eggs</option>
+                        <option value="GOOSE">goose</option>
+                        <option value="WEATHER-VANE">weather-vane</option>
+                        <option value="HAY-BALE">hay-bale</option>
+                        <option value="SEXY-FARMER">services of sexy-farmer (1 hour)</option>
+                        <option value="FARM-LADY">farm-lady (not sure what you'll get)</option>
+                        <option value="PS5">PS5</option>
+                        <option value="SPACE-MONKEY">astro space-monkey</option>
+                        <option value="PIKACHU">pikachu</option>
                     </select>
                 </form>
             </div>
