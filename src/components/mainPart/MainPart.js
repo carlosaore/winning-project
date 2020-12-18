184157513816b1ae1ecb99251280e4d83a17da33
Viktoryia <kulinkovichva@gmail.com>
2020-12-18 13:14:05 +0300

    fixed main

--- src/components/mainPart/MainPart.js
+++ src/components/mainPart/MainPart.js
@@ -2,7 +2,7 @@ import './MainPart.css';
 import React from 'react';
 import ExchangeData from '../../assets/ExchangeData';
 import { Link } from 'react-router-dom';
-import icon from './arrows.png'
+import icon from './arrows.png';
 
 export default function MainPart(props) {
     return (
@@ -75,21 +75,22 @@ export default function MainPart(props) {
             </div>
             <div className="img-Container">
                 <img src={props.myProductIcon.default} alt="" />
-                <img id='arrows'src={icon} alt="arrows" />
+                <img id="arrows" src={icon} alt="arrows" />
                 <img src={props.desiredProductIcon.default} alt="" />
             </div>
-            <p className='text-area'>
-                { isNaN(props.myProductAmoun)
-                    ? "Please select what you want to trade"
-                    : `You can trade ${props.myProductAmount} ${props.myProduct} for ${props.desiredProductAmount} ${props.desiredProduct}. This is a ${props.tradeFairness} trade.`
-                }
+            <p className="text-area">
+                {isNaN(props.myProductAmount)
+                    ? 'Please select what you want to trade'
+                    : `You can trade ${props.myProductAmount} ${props.myProduct} for ${props.desiredProductAmount} ${props.desiredProduct}. This is a ${props.tradeFairness} trade.`}
             </p>
             <div className="lower-buttons-container">
                 {/* <button onClick={props.updateRatios}>Update ratios</button>
                 <button>Reverse trade</button>
                 <button>Ratio fluctuation</button> */}
                 <button className="show-other-trades-button">
-                    <Link className='link' to="/other_trades">Show Other trades</Link>
+                    <Link className="link" to="/other_trades">
+                        Show Other trades
+                    </Link>
                 </button>
             </div>
         </div>
