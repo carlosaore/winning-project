5ade9b1aebb0f258a60d6d6cbad404b7bd437485
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2020-12-18 11:58:28 +0100

    undo a commit made after the 30 minute mark

--- src/components/mainPart/MainPart.js
+++ src/components/mainPart/MainPart.js
@@ -78,7 +78,9 @@ export default function MainPart(props) {
                 <img src={props.desiredProductIcon.default} alt="" />
             </div>
             <p className="text-area">
-                {`You can trade ${props.myProductAmount} ${props.myProduct} for ${props.desiredProductAmount} ${props.desiredProduct}. This is a ${props.tradeFairness} trade.`}
+                {isNaN(props.myProductAmount)
+                    ? 'Please select what you want to trade'
+                    : `You can trade ${props.myProductAmount} ${props.myProduct} for ${props.desiredProductAmount} ${props.desiredProduct}. This is a ${props.tradeFairness} trade.`}
             </p>
             <div className="lower-buttons-container">
                 {/* <button onClick={props.updateRatios}>Update ratios</button>
